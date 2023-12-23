fe0976d05a2377fb733df32f18d51154069b4beb
Uvneshkumar <uvneshkumar@gmail.com>
2023-12-23 15:57:27 +0530

    Adapt for dropbox links

--- script1.sh
+++ script1.sh
@@ -4,6 +4,8 @@ if [ -f recovery.img.lz4 ];then
 	lz4 -B6 --content-size -f recovery.img.lz4 recovery.img
 fi
 
+find . -depth -name "recovery.img*" -exec sh -c 'f="{}"; mv -- "$f" "recovery.img"' \;
+
 off=$(grep -ab -o SEANDROIDENFORCE recovery.img |tail -n 1 |cut -d : -f 1)
 dd if=recovery.img of=r.img bs=4k count=$off iflag=count_bytes
 
